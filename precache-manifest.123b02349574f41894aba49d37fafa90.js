917e1ca3815ea8178ba4482c790b04f26b9c25a4
yunganw <46147701+yunganw@users.noreply.github.com>
2019-11-06 00:53:58 +0000

    Deploying to gh-pages from master 5591009521234048995bf58877315fd7b4c3b244

---
+++ precache-manifest.123b02349574f41894aba49d37fafa90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a090ac25e2626f9eaa9d554bf0fc5b",
+    "url": "/synonyms/index.html"
+  },
+  {
+    "revision": "56a157e98d2c77f19cee",
+    "url": "/synonyms/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2815ae4dee9d75948ba2",
+    "url": "/synonyms/static/js/2.8f155fc4.chunk.js"
+  },
+  {
+    "revision": "56a157e98d2c77f19cee",
+    "url": "/synonyms/static/js/main.cf56668c.chunk.js"
+  },
+  {
+    "revision": "79fe16034b7cd895dd51",
+    "url": "/synonyms/static/js/runtime-main.1cac2e50.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/synonyms/static/media/logo.25bf045c.svg"
+  }
+]);
